b03c593c4d9a3e9a0feb1f5ab1a4a902261fbf4a
KelliePetersen <kelliepetersen@outlook.com>
2020-07-08 15:00:33 +1000

    remove unused function

--- catalogue.js
+++ catalogue.js
@@ -1,11 +1,6 @@
 document.body.style.border = "5px solid green";
 let container = document.querySelector('.home__feeds');
 
-function getMovieTitleOnHover() {
-  let items = document.querySelector('.programs__item');
-  console.log(items);
-}
-
 container.addEventListener('mouseover', event => {
   if (event.target.tagName == "IMG") {
     console.log(event.target.alt);
