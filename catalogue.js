8b1e721c466669beaac6bb06d91c4cca01d42ae9
KelliePetersen <kelliepetersen@outlook.com>
2020-08-04 11:47:01 +1000

    fetch imdbrating of show and log to console

--- catalogue.js
+++ catalogue.js
@@ -4,5 +4,13 @@ let container = document.querySelector('.home__feeds');
 container.addEventListener('mouseover', event => {
   if (event.target.tagName == "IMG") {
     console.log(event.target.alt);
+    let newData = fetchShowData(event.target.alt);
+    newData.then(data => console.log(data.imdbRating));
   }
 });
+
+async function fetchShowData(title) {
+  let response = await fetch(`http://www.omdbapi.com/?apikey=436ad94b&t=${title}`);
+  let data = await response.json();
+  return data;
+}
