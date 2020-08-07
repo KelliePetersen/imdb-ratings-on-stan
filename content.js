57963d1b8bb904a2f50e90ce2fde492c619cd051
KelliePetersen <kelliepetersen@outlook.com>
2020-08-07 17:32:45 +1000

    style list for movie ratings

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 document.body.style.border = "5px solid red";
 const programDetails = document.querySelector(".program__details");
 const ratingsContainer = document.createElement("ul");
+ratingsContainer.setAttribute("class", "program__attributes");
 programDetails.appendChild(ratingsContainer);
 
 function getShowRatings() {
   let title = document.querySelector('.program__title--logo').alt;
   let data = fetchShowData(title);
-  data.then(data => displayShowRatings(data.imdbRating));
+  data.then(data => displayShowRatings(data.imdbRating, data.imdbVotes));
 }
 
 async function fetchShowData(title) {
@@ -15,9 +16,10 @@ async function fetchShowData(title) {
   return data;
 }
 
-function displayShowRatings(imdbRating) {
+function displayShowRatings(imdbRating, imdbVotes) {
   let listItem = document.createElement("li");
-  let listContent = document.createTextNode(`${imdbRating}`);
+  listItem.style.listStyleType = "none";
+  let listContent = document.createTextNode(`IMDb: ${imdbRating} out of ${imdbVotes} votes`);
   listItem.appendChild(listContent);
   ratingsContainer.appendChild(listItem);
 }
